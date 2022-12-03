a6830bbab29d64995f544a1018d8d87ed6000848
linaida <linaida66@gmail.com>
2022-12-03 09:50:10 +0100

    Ajout fichiers docker + day1input

--- src/Controller/DaysController.php
+++ src/Controller/DaysController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\EventDay1\MainService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Path;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -18,12 +19,11 @@ class DaysController extends AbstractController
     {
 
         $fs = new Filesystem();
-        $inputPath = Path::makeAbsolute( join(DIRECTORY_SEPARATOR,['var','files','day1input.txt']),$projectDir);
-        if ($fs->exists($inputPath)) {
-            $solution = $mainService->run($inputPath);
-
+        $inputPath = Path::makeAbsolute( join(DIRECTORY_SEPARATOR,['public','files','day1input.txt']),$projectDir);
+        if (!$fs->exists($inputPath)) {
+           throw new FileNotFoundException(sprintf('Fichier inexistant [%s]', $inputPath));
         }
-
+        $solution = $mainService->run($inputPath);
 
         return $this->json([
             'message' => sprintf('The solution of day 1 is [%s]', $solution),
